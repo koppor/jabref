ac42cf67581b8a3988f0d9873e073778610b66c0
Antal K <antalk2@gmail.com>
2021-04-12 09:16:16 +0200

    isNumberEntries, isSortByPosition renamed to getI...

--- src/test/java/org/jabref/logic/openoffice/OOBibStyleTest.java
+++ src/test/java/org/jabref/logic/openoffice/OOBibStyleTest.java
@@ -48,8 +48,8 @@ class OOBibStyleTest {
         assertFalse(style.isBoldCitations());
         assertFalse(style.getFormatCitations());
         assertFalse(style.isItalicCitations());
-        assertFalse(style.isNumberEntries());
-        assertFalse(style.isSortByPosition());
+        assertFalse(style.getIsNumberEntries());
+        assertFalse(style.getIsSortByPosition());
     }
 
     @Test
@@ -63,8 +63,8 @@ class OOBibStyleTest {
         assertFalse(style.isBoldCitations());
         assertFalse(style.getFormatCitations());
         assertFalse(style.isItalicCitations());
-        assertFalse(style.isNumberEntries());
-        assertFalse(style.isSortByPosition());
+        assertFalse(style.getIsNumberEntries());
+        assertFalse(style.getIsSortByPosition());
     }
 
     @Test
@@ -76,8 +76,8 @@ class OOBibStyleTest {
         assertFalse(style.isBoldCitations());
         assertFalse(style.getFormatCitations());
         assertFalse(style.isItalicCitations());
-        assertTrue(style.isNumberEntries());
-        assertTrue(style.isSortByPosition());
+        assertTrue(style.getIsNumberEntries());
+        assertTrue(style.getIsSortByPosition());
     }
 
     @Test
