515a450e962dd3a8bb7c2b1b8201e310b88b2835
Antal K <antalk2@gmail.com>
2021-04-30 19:47:18 +0200

    document OOFormat methods

--- src/main/java/org/jabref/logic/oostyle/OOFormat.java
+++ src/main/java/org/jabref/logic/oostyle/OOFormat.java
@@ -48,28 +48,61 @@ public class OOFormat {
         return formattedText;
     }
 
+    /**
+     * Mark {@code s} as using a character locale known to OO.
+     *
+     * @param locale language[-country[-territory]]
+     *
+     * https://www.openoffice.org/api/docs/common/ref/com/sun/star/lang/Locale.html
+     *
+     * The country part is optional.
+     *
+     * The territory part is not only optional, the allowed "codes are
+     * vendor and browser-specific", so probably best to avoid them if possible.
+     *
+     */
     public static OOFormattedText setLocale(OOFormattedText s, String locale) {
         return OOFormattedText.fromString(String.format("<locale value=\"%s\">", locale)
                                           + s.asString()
                                           + "</locale>");
     }
 
+    /**
+     * Mark {@code s} as using the character locale "zxx", which means
+     * "no language", "no linguistic content".
+     *
+     * Used around citation marks, probably to turn off spellchecking.
+     *
+     */
     public static OOFormattedText setLocaleNone(OOFormattedText s) {
         return OOFormat.setLocale(s, "zxx");
     }
 
+    /**
+     * Mark {@code s} using a character style {@code charStyle}
+     *
+     * @param charStyle Name of a character style known to OO. May be
+     * empty for "Standard", which in turn means do not override any properties.
+     *
+     */
     public static OOFormattedText setCharStyle(OOFormattedText s, String charStyle) {
         return OOFormattedText.fromString(String.format("<font class=\"%s\">", charStyle)
                                           + s.asString()
                                           + "</font>");
     }
 
+    /**
+     * Mark {@code s} as part of a paragraph with style {@code paraStyle}
+     */
     public static OOFormattedText paragraph(OOFormattedText s, String paraStyle) {
         return OOFormattedText.fromString(String.format("<p class=\"%s\">", paraStyle)
                                           + s.asString()
                                           + "</p>");
     }
 
+    /**
+     * Mark {@code s} as part of a paragraph.
+     */
     public static OOFormattedText paragraph(OOFormattedText s) {
         return OOFormattedText.fromString("<p>"
                                           + s.asString()
