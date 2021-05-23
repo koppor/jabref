fb8ff9162307ebaa8b07eb44933da5b125eecf19
Antal K <antalk2@gmail.com>
2021-05-23 15:04:02 +0200

    add OOListUtil.zip

--- src/main/java/org/jabref/model/oostyle/OOListUtil.java
+++ src/main/java/org/jabref/model/oostyle/OOListUtil.java
@@ -1,8 +1,10 @@
 package org.jabref.model.oostyle;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -11,6 +13,19 @@ public class OOListUtil {
 
     public static <T, U> List<U> map(List<T> list, Function<T, U> fun) {
         return list.stream().map(e -> fun.apply(e)).collect(Collectors.toList());
+    }
+
+    public static <A, B, R> List<R> zip(List<A> a,
+                                        List<B> b,
+                                        BiFunction<? super A, ? super B, R> fun) {
+        if (a.size() != b.size()) {
+            throw new RuntimeException("a.size != b.size");
+        }
+        List<R> result = new ArrayList<>(a.size());
+        for (int i = 0; i < a.size(); i++) {
+            result.add(fun.apply(a.get(i), b.get(i)));
+        }
+        return result;
     }
 
     /** Integers 0..(n-1) */
