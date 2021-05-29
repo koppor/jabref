e8a4b643e8acb513855c5801aa04fcb38f830234
Antal K <antalk2@gmail.com>
2021-05-30 00:04:53 +0200

    add TimeLap.java

---
+++ src/main/java/org/jabref/model/openoffice/notforproduction/TimeLap.java
@@ -0,0 +1,28 @@
+package org.jabref.model.openoffice.notforproduction;
+
+/*
+ * Measure execution time.
+ */
+public class TimeLap {
+
+    /*
+     * Usage:
+     * long startTime = TimeLap.start();
+     * // call to measure
+     * startTime = TimeLap.now("label1", startTime);
+     * // call to measure
+     * startTime = TimeLap.now("label2", startTime);
+     */
+    // return time (nanoSeconds) for timing
+    public static long start() {
+        return System.nanoTime();
+    }
+
+    // return time (nanoSeconds) for next timing
+    public static long now(String label, long startTime) {
+        long endTime = System.nanoTime();
+        long duration = (endTime - startTime);  //divide by 1000000 to get milliseconds.
+        System.out.printf("%-40s: %.3f ms\n", label, duration /1000000.0);
+        return endTime;
+    }
+}
