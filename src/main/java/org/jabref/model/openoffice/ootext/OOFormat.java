cc8e60691b2cb31222834db6f5cfed71af7805a5
Antal K <antalk2@gmail.com>
2021-06-06 22:12:36 +0200

    rename s to ootext

--- src/main/java/org/jabref/model/openoffice/ootext/OOFormat.java
+++ src/main/java/org/jabref/model/openoffice/ootext/OOFormat.java
@@ -3,7 +3,7 @@ package org.jabref.model.openoffice.ootext;
 public class OOFormat {
 
     /**
-     * Mark {@code s} as using a character locale known to OO.
+     * Mark {@code ootext} as using a character locale known to OO.
      *
      * @param locale language[-country[-territory]]
      *
@@ -15,50 +15,50 @@ public class OOFormat {
      * vendor and browser-specific", so probably best to avoid them if possible.
      *
      */
-    public static OOText setLocale(OOText s, String locale) {
-        return OOText.fromString(String.format("<span lang=\"%s\">", locale) + s.asString() + "</span>");
+    public static OOText setLocale(OOText ootext, String locale) {
+        return OOText.fromString(String.format("<span lang=\"%s\">", locale) + ootext.asString() + "</span>");
     }
 
     /**
-     * Mark {@code s} as using the character locale "zxx", which means
+     * Mark {@code ootext} as using the character locale "zxx", which means
      * "no language", "no linguistic content".
      *
      * Used around citation marks, probably to turn off spellchecking.
      *
      */
-    public static OOText setLocaleNone(OOText s) {
-        return OOFormat.setLocale(s, "zxx");
+    public static OOText setLocaleNone(OOText ootext) {
+        return OOFormat.setLocale(ootext, "zxx");
     }
 
     /**
-     * Mark {@code s} using a character style {@code charStyle}
+     * Mark {@code ootext} using a character style {@code charStyle}
      *
      * @param charStyle Name of a character style known to OO. May be
      * empty for "Standard", which in turn means do not override any properties.
      *
      */
-    public static OOText setCharStyle(OOText s, String charStyle) {
+    public static OOText setCharStyle(OOText ootext, String charStyle) {
         return OOText.fromString(String.format("<span oo:CharStyleName=\"%s\">", charStyle)
-                                          + s.asString()
+                                          + ootext.asString()
                                           + "</span>");
     }
 
     /**
-     * Mark {@code s} as part of a paragraph with style {@code paraStyle}
+     * Mark {@code ootext} as part of a paragraph with style {@code paraStyle}
      */
-    public static OOText paragraph(OOText s, String paraStyle) {
+    public static OOText paragraph(OOText ootext, String paraStyle) {
         if (paraStyle == null || "".equals(paraStyle)) {
-            return paragraph(s);
+            return paragraph(ootext);
         }
         String startTag = String.format("<p oo:ParaStyleName=\"%s\">", paraStyle);
-        return OOText.fromString(startTag + s.asString() + "</p>");
+        return OOText.fromString(startTag + ootext.asString() + "</p>");
     }
 
     /**
-     * Mark {@code s} as part of a paragraph.
+     * Mark {@code ootext} as part of a paragraph.
      */
-    public static OOText paragraph(OOText s) {
-        return OOText.fromString("<p>" + s.asString() + "</p>");
+    public static OOText paragraph(OOText ootext) {
+        return OOText.fromString("<p>" + ootext.asString() + "</p>");
     }
 
     /**
@@ -66,8 +66,8 @@ public class OOFormat {
      * as label to a reference mark.
      */
     public static OOText formatReferenceToPageNumberOfReferenceMark(String referencMarkName) {
-        String s = String.format("<oo:referenceToPageNumberOfReferenceMark target=\"%s\">",
-                                 referencMarkName);
-        return OOText.fromString(s);
+        String string = String.format("<oo:referenceToPageNumberOfReferenceMark target=\"%s\">",
+                                      referencMarkName);
+        return OOText.fromString(string);
     }
  }
