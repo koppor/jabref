82492fda6f90ada38bf6d068d7d6cabd864c39b9
Oliver Kopp <kopp.dev@gmail.com>
2023-03-17 20:39:33 +0100

    Really remove Markdown extension

--- src/main/java/org/jabref/logic/layout/format/MarkdownFormatter.java
+++ src/main/java/org/jabref/logic/layout/format/MarkdownFormatter.java
@@ -1,16 +1,12 @@
 package org.jabref.logic.layout.format;
 
-import java.util.List;
 import java.util.Objects;
 
 import org.jabref.logic.layout.LayoutFormatter;
 
-import com.vladsch.flexmark.ext.gfm.strikethrough.StrikethroughExtension;
-import com.vladsch.flexmark.ext.gfm.tasklist.TaskListExtension;
 import com.vladsch.flexmark.html.HtmlRenderer;
 import com.vladsch.flexmark.parser.Parser;
 import com.vladsch.flexmark.util.ast.Node;
-import com.vladsch.flexmark.util.data.MutableDataSet;
 
 public class MarkdownFormatter implements LayoutFormatter {
 
@@ -18,15 +14,8 @@ public class MarkdownFormatter implements LayoutFormatter {
     private final HtmlRenderer renderer;
 
     public MarkdownFormatter() {
-        MutableDataSet options = new MutableDataSet();
-        // in case a new extension is added here, the depedency has to be added to build.gradle, too.
-        options.set(Parser.EXTENSIONS, List.of(
-                StrikethroughExtension.create(),
-                TaskListExtension.create()
-        ));
-
-        parser = Parser.builder(options).build();
-        renderer = HtmlRenderer.builder(options).build();
+        parser = Parser.builder().build();
+        renderer = HtmlRenderer.builder().build();
     }
 
     @Override
