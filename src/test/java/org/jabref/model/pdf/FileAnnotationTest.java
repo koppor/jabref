e76cf43a9493ac952577eff0f405402098a72382
Oliver Kopp <kopp.dev@gmail.com>
2023-05-05 01:29:24 +0200

    Enable WriteOctalValuesAsDecimal

--- src/test/java/org/jabref/model/pdf/FileAnnotationTest.java
+++ src/test/java/org/jabref/model/pdf/FileAnnotationTest.java
@@ -15,21 +15,21 @@ public class FileAnnotationTest {
     public void testParseDateMinusBeforeTimezone() {
         String dateString = "D:20170512224019-03'00'";
         LocalDateTime date = FileAnnotation.extractModifiedTime(dateString);
-        assertEquals(LocalDateTime.of(2017, 05, 12, 22, 40, 19), date);
+        assertEquals(LocalDateTime.of(2017, 5, 12, 22, 40, 19), date);
     }
 
     @Test
     public void testParseDatePlusBeforeTimezone() {
         String dateString = "D:20170512224019+03'00'";
         LocalDateTime date = FileAnnotation.extractModifiedTime(dateString);
-        assertEquals(LocalDateTime.of(2017, 05, 12, 22, 40, 19), date);
+        assertEquals(LocalDateTime.of(2017, 5, 12, 22, 40, 19), date);
     }
 
     @Test
     public void testParseDateNoTimezone() {
         String dateString = "D:20170512224019";
         LocalDateTime date = FileAnnotation.extractModifiedTime(dateString);
-        assertEquals(LocalDateTime.of(2017, 05, 12, 22, 40, 19), date);
+        assertEquals(LocalDateTime.of(2017, 5, 12, 22, 40, 19), date);
     }
 
     @Test
