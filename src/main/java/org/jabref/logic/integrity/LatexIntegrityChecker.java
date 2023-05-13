bf882c095bb43c9b7890395b2f09af6291e6851f
Julian Kirsch <kirsch.julian@yahoo.com>
2023-05-13 20:25:24 +0200

    Added error exclusions for snuggletex and a changelog entry + style changes to make the linter happy

--- src/main/java/org/jabref/logic/integrity/LatexIntegrityChecker.java
+++ src/main/java/org/jabref/logic/integrity/LatexIntegrityChecker.java
@@ -1,17 +1,32 @@
 package org.jabref.logic.integrity;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import org.jabref.logic.l10n.Localization;
 import org.jabref.model.entry.BibEntry;
 import org.jabref.model.entry.field.Field;
-import uk.ac.ed.ph.snuggletex.*;
+
+import uk.ac.ed.ph.snuggletex.ErrorCode;
+import uk.ac.ed.ph.snuggletex.InputError;
+import uk.ac.ed.ph.snuggletex.SnuggleEngine;
+import uk.ac.ed.ph.snuggletex.SnuggleInput;
+import uk.ac.ed.ph.snuggletex.SnuggleSession;
+import uk.ac.ed.ph.snuggletex.definitions.CoreErrorCode;
 import uk.ac.ed.ph.snuggletex.definitions.CoreErrorGroup;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
 public class LatexIntegrityChecker implements EntryChecker {
+
+    private static Set<ErrorCode> excludedErrors = new HashSet<>(
+            Arrays.asList(
+                    CoreErrorCode.TTEG04  // # only allowed inside and command/environment definitions
+            )
+    );
 
     @Override
     public List<IntegrityMessage> check(BibEntry entry) {
@@ -20,6 +35,9 @@ public class LatexIntegrityChecker implements EntryChecker {
         SnuggleEngine engine = new SnuggleEngine();
         SnuggleSession session = engine.createSession();
         session.getConfiguration().setFailingFast(true);
+
+        // example of how we could add commands that are not yet present
+        // engine.getPackages().get(0).addComplexCommandOneArg("text", false, ALL_MODES,LR, StyleDeclarationInterpretation.NORMALSIZE, null, TextFlowContext.ALLOW_INLINE);
 
         for (Map.Entry<Field, String> field : entry.getFieldMap().entrySet()) {
             SnuggleInput input = new SnuggleInput(field.getValue());
@@ -31,9 +49,9 @@ public class LatexIntegrityChecker implements EntryChecker {
             if (!session.getErrors().isEmpty()) {
                 InputError error = session.getErrors().get(0);
                 ErrorCode errorCode = error.getErrorCode();
-                // exclude Dom building errors as this functionality is not used, and we do not have the translations
-                if (!errorCode.getErrorGroup().equals(CoreErrorGroup.TDE)) {
-                    //todo filter for specific errors
+                // exclude all Dom building errors as this functionality is not used, and we do not have the translations
+                // further exclude individual errors
+                if (!errorCode.getErrorGroup().equals(CoreErrorGroup.TDE) && !excludedErrors.contains(errorCode)) {
                     String message = Localization.lang(errorCode.getName(), error.getArguments());
                     results.add(new IntegrityMessage(message, entry, field.getKey()));
                     session.getErrors().clear();
