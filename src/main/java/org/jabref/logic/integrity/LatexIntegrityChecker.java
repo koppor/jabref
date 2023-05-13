07e4fc026d5743233734b1b0d22bb157b9bdaa6e
Julian Kirsch <kirsch.julian@yahoo.com>
2023-05-13 19:30:52 +0200

    LatexIntegrityChecker is now invoked on IntegrityCheck, it further uses the jabref properties for translations and excludes dom building errors

--- src/main/java/org/jabref/logic/integrity/LatexIntegrityChecker.java
+++ src/main/java/org/jabref/logic/integrity/LatexIntegrityChecker.java
@@ -1,5 +1,6 @@
 package org.jabref.logic.integrity;
 
+import org.jabref.logic.l10n.Localization;
 import org.jabref.model.entry.BibEntry;
 import org.jabref.model.entry.field.Field;
 import uk.ac.ed.ph.snuggletex.ErrorCode;
@@ -7,7 +8,6 @@ import uk.ac.ed.ph.snuggletex.SnuggleEngine;
 import uk.ac.ed.ph.snuggletex.SnuggleInput;
 import uk.ac.ed.ph.snuggletex.SnuggleSession;
 import uk.ac.ed.ph.snuggletex.definitions.CoreErrorGroup;
-import uk.ac.ed.ph.snuggletex.definitions.CorePackageDefinitions;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -15,6 +15,7 @@ import java.util.List;
 import java.util.Map;
 
 public class LatexIntegrityChecker implements EntryChecker {
+
     @Override
     public List<IntegrityMessage> check(BibEntry entry) {
         List<IntegrityMessage> results = new ArrayList<>();
@@ -30,9 +31,10 @@ public class LatexIntegrityChecker implements EntryChecker {
             }
             if (!session.getErrors().isEmpty()) {
                 ErrorCode code = session.getErrors().get(0).getErrorCode();
-                // filter only for tokenization errors
-                if (code.getErrorGroup().equals(CoreErrorGroup.TTE)) {
-                    String message = CorePackageDefinitions.getPackage().getErrorMessageBundle().getString(code.getName());
+                // exclude Dom building errors as this functionality is not used, and we do not have the translations
+                if (!code.getErrorGroup().equals(CoreErrorGroup.TDE)) {
+                    String message = Localization.lang(code.getName());
+                    //String message = CorePackageDefinitions.getPackage().getErrorMessageBundle().getString(code.getName());
                     results.add(new IntegrityMessage(message, entry, field.getKey()));
                     session.getErrors().clear();
                 }
