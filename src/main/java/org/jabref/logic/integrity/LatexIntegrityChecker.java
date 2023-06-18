29d311153c9db60a6eb989afd91193ad0934b090
Julian Kirsch <kirsch.julian@yahoo.com>
2023-06-18 23:42:11 +0200

    Applied suggested Changes and did some Refactoring
    Localization now uses a string from jabrefs properties to wrap the internal error messages.
    Local fields have been made static members in order to improve performance with large bib files. We no longer instanciate an Engine and Session per bib entry.

--- src/main/java/org/jabref/logic/integrity/LatexIntegrityChecker.java
+++ src/main/java/org/jabref/logic/integrity/LatexIntegrityChecker.java
@@ -1,17 +1,20 @@
 package org.jabref.logic.integrity;
 
 import java.io.IOException;
+import java.text.MessageFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.ResourceBundle;
 import java.util.Set;
 
 import org.jabref.logic.l10n.Localization;
 import org.jabref.model.entry.BibEntry;
 import org.jabref.model.entry.field.Field;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import uk.ac.ed.ph.snuggletex.ErrorCode;
 import uk.ac.ed.ph.snuggletex.InputError;
 import uk.ac.ed.ph.snuggletex.SnuggleEngine;
@@ -22,41 +25,44 @@ import uk.ac.ed.ph.snuggletex.definitions.CoreErrorGroup;
 
 public class LatexIntegrityChecker implements EntryChecker {
 
-    private static Set<ErrorCode> excludedErrors = new HashSet<>(
-            Arrays.asList(
-                    CoreErrorCode.TTEG04  // # only allowed inside and command/environment definitions
-            )
-    );
+    private static final Logger LOGGER = LoggerFactory.getLogger(SnuggleSession.class);
+    private static final SnuggleEngine ENGINE = new SnuggleEngine();
+    private static final SnuggleSession SESSION = ENGINE.createSession();
+    private static final ResourceBundle ERROR_MESSAGES = ENGINE.getPackages().get(0).getErrorMessageBundle();
+    private static final Set<ErrorCode> EXCLUDED_ERRORS = new HashSet<>();
+
+    static {
+        // Static configuration.
+        SESSION.getConfiguration().setFailingFast(true);
+
+        // '#' only allowed inside and command/environment definitions.
+        EXCLUDED_ERRORS.add(CoreErrorCode.TTEG04);
+    }
 
     @Override
     public List<IntegrityMessage> check(BibEntry entry) {
         List<IntegrityMessage> results = new ArrayList<>();
 
-        SnuggleEngine engine = new SnuggleEngine();
-        SnuggleSession session = engine.createSession();
-        session.getConfiguration().setFailingFast(true);
-
-        // example of how we could add commands that are not yet present
-        // engine.getPackages().get(0).addComplexCommandOneArg("text", false, ALL_MODES,LR, StyleDeclarationInterpretation.NORMALSIZE, null, TextFlowContext.ALLOW_INLINE);
-
         for (Map.Entry<Field, String> field : entry.getFieldMap().entrySet()) {
             SnuggleInput input = new SnuggleInput(field.getValue());
             try {
-                session.parseInput(input);
+                SESSION.parseInput(input);
             } catch (IOException e) {
-                e.printStackTrace();
+                LOGGER.error("Error at parsing", e);
             }
-            if (!session.getErrors().isEmpty()) {
-                InputError error = session.getErrors().get(0);
+            if (!SESSION.getErrors().isEmpty()) {
+                InputError error = SESSION.getErrors().get(0);
                 ErrorCode errorCode = error.getErrorCode();
-                // exclude all Dom building errors as this functionality is not used, and we do not have the translations
-                // further exclude individual errors
-                if (!errorCode.getErrorGroup().equals(CoreErrorGroup.TDE) && !excludedErrors.contains(errorCode)) {
-                    String message = Localization.lang(errorCode.getName(), error.getArguments());
-                    results.add(new IntegrityMessage(message, entry, field.getKey()));
-                    session.getErrors().clear();
+                // Exclude all DOM building errors as this functionality is not used.
+                // Further, exclude individual errors.
+                if (!errorCode.getErrorGroup().equals(CoreErrorGroup.TDE) && !EXCLUDED_ERRORS.contains(errorCode)) {
+                    String snuggletexMessagePattern = ERROR_MESSAGES.getString(errorCode.getName());
+                    String snuggletexErrorMessage = MessageFormat.format(snuggletexMessagePattern, error.getArguments());
+                    String jabrefMessageWrapper = Localization.lang("LaTeX Parsing Error: %0", snuggletexErrorMessage);
+                    results.add(new IntegrityMessage(jabrefMessageWrapper, entry, field.getKey()));
                 }
             }
+            SESSION.reset();
         }
         return results;
     }
